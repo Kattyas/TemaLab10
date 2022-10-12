4caed53317971ab13dfa2f11590969a88fbdfcd1
Catalin <katalinus_katty@yahoo.com>
2022-10-12 22:38:27 +0300

    TemaLab10

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,21 @@
 public class Main {
     public static void main(String[] args) {
         arataNumeleMeu("Catalin");
+        int a = -5;
+        int b = -3;
+        int c = 2;
+        int d = 3;
+        int e = 5;
+        int f = 6;
+        int g = 8;
+        int h = 9;
+        int i = 15;
+        int j = 20;
+        int k = 55;
     }
     public static void arataNumeleMeu(String nume){
         System.out.println(" Salut "+"\n" + nume +"!");
-
     }
 
-}
+
+    }
