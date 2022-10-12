40cc309cc43339c152996e7ffa70badacfcaf22b
Catalin <katalinus_katty@yahoo.com>
2022-10-12 21:29:57 +0300

    TemaLab10

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        arataNumeleMeu("Catalin");
+    }
+    public static void arataNumeleMeu(String nume){
+        System.out.println(" Salut "+"\n" + nume +"!");
+
+    }
+
+}
